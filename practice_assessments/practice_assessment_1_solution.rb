d5c1892242fe501585a52f069034be0c5748c681
sewinter <samuelewinter@gmail.com>
2016-12-20 17:26:48 -0800

    write practice 1 test suite

--- practice_assessments/practice_assessment_1_solution.rb
+++ practice_assessments/practice_assessment_1_solution.rb
@@ -1,11 +1,6 @@
-# Anagrams
-
 def anagrams?(str1, str2)
   str1.chars.sort == str2.chars.sort
 end
-
-# ------------------------------------------------------------------------------
-# Isogram Matcher
 
 def isogram_matcher(isogram1, isogram2)
   idx_match = 0
@@ -22,9 +17,6 @@ def isogram_matcher(isogram1, isogram2)
   [idx_match, letter_match]
 end
 
-# ------------------------------------------------------------------------------
-# Panoramic Pairs
-
 def panoramic_pairs(landmarks)
   pairs = []
 
@@ -35,9 +27,6 @@ def panoramic_pairs(landmarks)
 
   pairs
 end
-
-# ------------------------------------------------------------------------------
-# Xbonacci
 
 def xbonacci(starting_sequence, n)
   result = starting_sequence
@@ -60,3 +49,107 @@ end
 def array_sum(array)
   array.reduce(0){ |sum, n| sum + n }
 end
+
+
+## TEST SUITE ##
+
+$success_count = 0
+$failure_count = 0
+
+def deep_dup(arr)
+  arr.inject([]) { |acc, el| el.is_a?(Array) ? acc << deep_dup(el) : acc << el }
+end
+
+def note_success(returned, invocation, expectation)
+  puts "success: #{invocation} => #{expectation}"
+  $success_count += 1
+end
+
+def note_failure(returned, invocation, expectation)
+  puts "failure: #{invocation}: expected #{expectation}, returned #{returned}"
+  $failure_count += 1
+end
+
+def format_args(args)
+  o_args = deep_dup(args)
+  o_args.map! do |arg|
+    arg = prettify(arg)
+    arg.class == Array ? arg.to_s : arg
+  end
+  o_args.join(', ')
+end
+
+def prettify(statement)
+  case statement
+  when Float
+    statement.round(5)
+  when String
+    "\"#{statement}\""
+  when NilClass
+    "nil"
+  else
+    statement
+  end
+end
+
+def equality_test(returned, invocation, expectation)
+  if returned == expectation && returned.class == expectation.class
+    note_success(returned, invocation, expectation)
+  else
+    note_failure(returned, invocation, expectation)
+  end
+end
+
+def identity_test(returned, invocation, expectation, args)
+  if returned.__id__ == args[0].__id__
+    equality_test(returned, invocation, expectation)
+  else
+    puts "failure: #{invocation}: You did not mutate the original array!"
+    $failure_count += 1
+  end
+end
+
+def method_missing(method_name, *args)
+  method_name = method_name.to_s
+  expectation = args[-1]
+  args = args[0...-1]
+  if method_name.start_with?("test_")
+    tested_method = method_name[5..-1]
+    print_test(tested_method, args, expectation)
+  else
+    method_name = method_name.to_sym
+    super
+  end
+end
+
+def print_test(method_name, args, expectation)
+  returned = self.send(method_name, *args)
+  returned = prettify(returned)
+  expectation = prettify(expectation)
+  args_statement = format_args(args)
+  invocation = "#{method_name}(#{args_statement})"
+  method_name.include?("!") ? identity_test(returned, invocation, expectation, args) : equality_test(returned, invocation, expectation)
+  rescue Exception => e
+    puts "failure: #{invocation} threw #{e}"
+    puts e.backtrace.select {|t| !t.include?("method_missing") && !t.include?("print_test")}
+    $failure_count += 1
+end
+
+landmarks_1 = ["House", "Horse"]
+pairs_1 = [["House", "Horse"], ["Horse", "House"]]
+landmarks_2 = ["Tree", "Mountain", "Ocean", "Cottage"]
+pairs_2 = [["Tree", "Mountain"], ["Mountain", "Ocean"], ["Ocean", "Cottage"], ["Cottage", "Tree"]]
+
+test_anagrams?("desert", "rested", true)
+test_anagrams?("banana", "fofanna", false)
+test_isogram_matcher("or", "go", [0,1])
+test_isogram_matcher("unpredictably", "hydromagnetic", [1,8])
+test_panoramic_pairs(landmarks_1, pairs_1)
+test_panoramic_pairs(landmarks_2, pairs_2)
+test_xbonacci([1, 1], 5, [1, 1, 2, 3, 5])
+test_xbonacci([1, 1, 1, 1], 8, [1, 1, 1, 1, 4, 7, 13, 25])
+puts
+puts "TOTAL CORRECT: #{$success_count} / #{$success_count + $failure_count}"
+puts "TOTAL FAILURES: #{$failure_count}"
+$success_count = 0
+$failure_count = 0
