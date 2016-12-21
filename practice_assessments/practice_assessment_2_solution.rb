6bc780cac52ccf669c955664f8a786a71cccd200
sewinter <samuelewinter@gmail.com>
2016-12-21 10:33:12 -0800

    write practice 2 test suite

--- practice_assessments/practice_assessment_2_solution.rb
+++ practice_assessments/practice_assessment_2_solution.rb
@@ -1,5 +1,3 @@
-# Remove Letter 'A'
-
 def remove_letter_a(words)
   words.map { |word| remove_a(word) }
 end
@@ -8,8 +6,6 @@ def remove_a(word)
   word.gsub("a", "")
 end
 
-# ------------------------------------------------------------------------------
-# Abundant
 
 def abundant?(num)
   divisors = find_divisors(num)
@@ -25,8 +21,6 @@ def array_sum(array)
   array.reduce(0) { |sum, n| sum + n }
 end
 
-# ------------------------------------------------------------------------------
-# Greatest Common Factor
 
 def greatest_common_factor(first_number, second_number)
   smaller_num = [first_number, second_number].min
@@ -40,8 +34,6 @@ def greatest_common_factor(first_number, second_number)
   end
 end
 
-# ------------------------------------------------------------------------------
-# Word With Most Repeats
 
 def word_with_most_repeats(sentence)
   sentence.split.max_by { |word| count_repeats(word) }
@@ -53,3 +45,108 @@ def count_repeats(word)
     word.count(char) > 1
   end
 end
+
+
+## TEST SUITE ##
+
+$success_count = 0
+$failure_count = 0
+
+def deep_dup(arr)
+  arr.inject([]) { |acc, el| el.is_a?(Array) ? acc << deep_dup(el) : acc << el }
+end
+
+def note_success(returned, invocation, expectation)
+  puts "success: #{invocation} => #{expectation}"
+  $success_count += 1
+end
+
+def note_failure(returned, invocation, expectation)
+  puts "failure: #{invocation}: expected #{expectation}, returned #{returned}"
+  $failure_count += 1
+end
+
+def format_args(args)
+  o_args = deep_dup(args)
+  o_args.map! do |arg|
+    arg = prettify(arg)
+    arg.class == Array ? arg.to_s : arg
+  end
+  o_args.join(', ')
+end
+
+def prettify(statement)
+  case statement
+  when Float
+    statement.round(5)
+  when String
+    "\"#{statement}\""
+  when NilClass
+    "nil"
+  else
+    statement
+  end
+end
+
+def equality_test(returned, invocation, expectation)
+  if returned == expectation && returned.class == expectation.class
+    note_success(returned, invocation, expectation)
+  else
+    note_failure(returned, invocation, expectation)
+  end
+end
+
+def identity_test(returned, invocation, expectation, args)
+  if returned.__id__ == args[0].__id__
+    equality_test(returned, invocation, expectation)
+  else
+    puts "failure: #{invocation}: You did not mutate the original array!"
+    $failure_count += 1
+  end
+end
+
+def method_missing(method_name, *args)
+  method_name = method_name.to_s
+  expectation = args[-1]
+  args = args[0...-1]
+  if method_name.start_with?("test_")
+    tested_method = method_name[5..-1]
+    print_test(tested_method, args, expectation)
+  else
+    method_name = method_name.to_sym
+    super
+  end
+end
+
+def print_test(method_name, args, expectation)
+  returned = self.send(method_name, *args)
+  returned = prettify(returned)
+  expectation = prettify(expectation)
+  args_statement = format_args(args)
+  invocation = "#{method_name}(#{args_statement})"
+  method_name.include?("!") ? identity_test(returned, invocation, expectation, args) : equality_test(returned, invocation, expectation)
+  rescue Exception => e
+    puts "failure: #{invocation} threw #{e}"
+    puts e.backtrace.select {|t| !t.include?("method_missing") && !t.include?("print_test")}
+    $failure_count += 1
+end
+
+words_1 = ["blueberry", "apple", "banana", "peach"]
+without_a_1 = ["blueberry", "pple", "bnn", "pech"]
+
+words_2 = ["syllabus", "smirk", "salamander", "saaaaaaa"]
+without_a_2 = ["syllbus", "smirk", "slmnder", "s"]
+
+test_remove_letter_a(words_1, without_a_1)
+test_remove_letter_a(words_2, without_a_2)
+test_abundant?(10001, false)
+test_abundant?(20000, true)
+test_greatest_common_factor(4, 7, 1)
+test_greatest_common_factor(10, 15, 5)
+test_word_with_most_repeats("good luck", "good")
+test_word_with_most_repeats("what if there is a tie betwixt words", "there")
+puts
+puts "TOTAL CORRECT: #{$success_count} / #{$success_count + $failure_count}"
+puts "TOTAL FAILURES: #{$failure_count}"
+$success_count = 0
+$failure_count = 0
